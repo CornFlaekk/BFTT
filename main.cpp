951a421be874a2f37e9dff20f6490c2132518ebf
CornFlaekk <roy062002@gmail.com>
2023-12-18 00:35:39 +0100

    Fixes #2

--- main.cpp
+++ main.cpp
@@ -4,24 +4,47 @@
 
 using namespace std;
 
+string adjustValue(int preVal, int actVal);
+
 int main(){
     cout << "Type your text: \n";
     string lineRead;
     getline (cin, lineRead);
 
     string output = ">";
+    int preVal = 0;
+
     for (char c : lineRead){
-        output += '>';
-        for(int i = 0; i < int(c); i++){    //Increment byte up to ASCII corresponding value
-            output.append("+");
-        }
+        output.append(adjustValue(preVal, int(c)));
         output.append(".\t" ); //Output char
         output += c; //Clarify letter being written in output file
         output += '\n';
+        preVal = int(c);
     }
 
     ofstream outputFile("output.bf");
     outputFile << output;
     outputFile.close();
 
+    return 0;
+}
+
+/**
+ * @param preVal Value stored in the byte
+ * @param actVal Value desired in the byte
+ * 
+ * @return String which increases or decreases the byte value to the desired.
+*/
+string adjustValue(int preVal, int actVal){
+    string output;
+    if(actVal > preVal){
+        for(int i = preVal; i < actVal; i++){
+            output += '+';
+        }
+    }else{
+        for(int i = preVal; i > actVal; i--){
+            output += '-';
+        }
+    }
+    return output;
 }
